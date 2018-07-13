e619be27d7c7529170d4061ab4d00314e8c31d45
John Michael <ducnm.john98@gmail.com>
2018-07-13 23:22:11 +0700

    a

--- routes/check.js
+++ routes/check.js
@@ -5,7 +5,10 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   mongoose.model('questions').find({}, (err, result) => {
-    res.send(result);
+    res.render('check', {
+      result: result,
+    })
+    // res.send(result);
   })
 });
 
